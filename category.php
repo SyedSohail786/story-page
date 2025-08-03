9adfb50483976dc9cb8c86c57154091c5728c7f7
Syed Sohail <sohailsyed7867123@gmail.com>
2025-08-03 20:44:26 +0530

    added slugs to category url

--- category.php
+++ category.php
@@ -166,7 +166,7 @@ $stories = $stmt->fetchAll();
                     <p class="card-text text-truncate-3"><?= strip_tags(substr($s['content'], 0, 100)) ?>...</p>
                   </div>
                   <div class="card-footer bg-transparent border-0 pt-0">
-                    <a href="story.php?id=<?= $s['id'] ?>" class="btn btn-sm btn-orange w-100">
+                    <a href="story/<?= urlencode($s['slug']) ?>"  class="btn btn-sm btn-orange w-100">
                       Read Story
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right ms-1" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
